49509965e7d5ba5b9dec949316bbe166d23335cb
Benjaamiiin11 <Beijiamiao@gmail.com>
2025-09-24 00:08:36 +0800

    chore: 初始化模块的目录结构

---
+++ common/src/main/java/com/yukino/trip/common/login/LoginUser.java
@@ -0,0 +1,12 @@
+package com.yukino.trip.common.login;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class LoginUser {
+
+    private Long userId;
+    private String username;
+}
---
+++ common/src/main/java/com/yukino/trip/common/mybatisplus/MybatisMetaObjectHandler.java
@@ -0,0 +1,29 @@
+package com.yukino.trip.common.mybatisplus;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * ClassName: MybatisMetaObjectHandler
+ * Package: com.yukino.lease.common.mybatisplus
+ * Description:
+ *
+ * @Author YukinoshitaYukino
+ * @Create 2025/3/16 19:21
+ * @Version 1.0
+ */
+@Component
+public class MybatisMetaObjectHandler implements MetaObjectHandler {
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject, "createTime", Date.class, new Date());
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.strictUpdateFill(metaObject, "updateTime", Date.class, new Date());
+    }
+}
