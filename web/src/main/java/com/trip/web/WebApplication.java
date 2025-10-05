5997ca9269529bc276050fb1f28df367c60235a2
Avidity <2642350585@qq.com>
2025-10-05 23:35:00 +0800

    修改web模块结构

--- web/src/main/java/com/trip/web/WebApplication.java
+++ web/src/main/java/com/trip/web/WebApplication.java
@@ -1,15 +1,14 @@
-package com.yukino.trip.web;
+package com.trip.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Web服务启动类
  */
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.yukino.trip"})
+@ComponentScan(basePackages = {"com.trip"})
 public class WebApplication {
 
     public static void main(String[] args) {
